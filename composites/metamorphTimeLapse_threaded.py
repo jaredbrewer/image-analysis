35161ab6049eba64d78dc1a009fd9329cfdb55d5
Jared Brewer <jaredbrewer@me.com>
2023-02-15 11:55:18 -0500

    Additional layer of threading (for the individual times) = 2x gains.

--- composites/metamorphTimeLapse_threaded.py
+++ composites/metamorphTimeLapse_threaded.py
@@ -128,18 +128,35 @@ def metamorpher(names, chans, scenes, times, ext, colors, timelapse = True, mult
 		return title
 
 	def stiller(timelapse, combo_dict, name, chan, times, key, scene = "scene"):
-		stills = []
+
+		def projector(time, still, time_dict):
+			proj = ZProjector.run(still, "max")
+			time_dict[int(time)] = proj
+
 		if timelapse:
+			thread_dict = {}
+			time_dict = {}
 			for time in times:
 				still = path.join(basedir, "_".join([name, chan, "s" + str(scene), "t" + str(time) + "".join(ext)]))
 				if path.isfile(still):
 					still = ImagePlus(still)
 					if still.getNSlices() > 1:
-						still = ZProjector.run(still, "max")
-						stills.append(still)
+						thread = ConciseResult(target = projector, args = (time, still, time_dict))
+						thread_dict[int(time)] = thread
+						# Can I parallelize this for even more performance?
+						# still = ZProjector.run(still, "max")
+						# stills.append(still)
 					else:
-						stills.append(still)
-			if stills:
+						time_dict[int(time)] = still
+						# stills.append(still)
+			for thread in thread_dict.values():
+				thread.start()
+			for time, thread in thread_dict.items():
+				thread.join()
+			if time_dict:
+				stills = []
+				for time, proj in sorted(time_dict.items()):
+					stills.append(proj)
 				cat = Concatenator.run(stills)
 				return cat
 		else:
@@ -183,7 +200,7 @@ def metamorpher(names, chans, scenes, times, ext, colors, timelapse = True, mult
 			for key, chan in chans.items():
 				thread = ConciseResult(target = stiller, args = (timelapse, combo_dict, name, chan, times, key, scene))
 				thread_dict[key] = thread
-			main_dict[title] = thread
+			main_dict[title] = thread_dict
 			# We now have a dictionary of dictionaries: A main dictionary with keys as scene titles and values containing a dictionary with channels and their associated unique threads.
 		combos = {}
 		for title, threads in main_dict.items():
@@ -197,31 +214,12 @@ def metamorpher(names, chans, scenes, times, ext, colors, timelapse = True, mult
 		for title, combo_dict in combos.items():
 			merge_and_save(combo_dict, colors, title, outputdir, opener)
 
-	threads = []
-
 	for name in names:
 		main_dict = {}
 		if timelapse and multiscene:
-			for scene in scenes:
-				thread_dict = {}
-				combo_dict = {}
-				title = titler(nder, name, scene)
-				for key, chan in chans.items():
-					thread = ConciseResult(target = stiller, args = (timelapse, combo_dict, name, chan, times, key, scene))
-					thread_dict[key] = thread
-				main_dict[title] = thread_dict
-			combos = {}
-			for title, threads in main_dict.items():
-				chan_dict = {}
-				for channel, thread in threads.items():
-					thread.start()
-				for channel, thread in threads.items():
-					thread.join()
-					chan_dict[channel] = thread.result
-				combos[title] = chan_dict
-			for title, combo_dict in combos.items():
-				merge_and_save(combo_dict, colors, title, outputdir, opener)
+			threader(main_dict, name, chans, scenes, nder)
 		elif timelapse:
+			# Threading does not seem overly beneficial in a single stage position time lapse -- you could in theory parallelize the channels but the real-world benefit seems pretty small.
 			title = name
 			combo_dict = {}
 			for key, chan in chans.items():
